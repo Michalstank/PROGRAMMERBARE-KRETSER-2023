250bc63e222db4641c3450bd6de60262e103fd96
Michal <31627253+Michalstank@users.noreply.github.com>
2023-11-15 15:04:43 +0100

    Update UART.vhd

--- CapstonePoject/UART.vhd
+++ CapstonePoject/UART.vhd
@@ -16,6 +16,8 @@ entity UART is
 		
 		rx_signal: in std_logic;
 		
+		tx_signal: out std_logic := '1';
+		
 		-- Display of recived data
 		display_hex1,
 		display_hex2: out std_logic_vector(7 downto 0)
@@ -26,8 +28,11 @@ end entity;
 architecture UART_ARCH of UART is
 	-------------------------- STATES --------------------------
 	
-	type state_type is (idle,bit_sampling,byte_processing,display_state, buffer_state);
-	signal state:  state_type;
+	type rx_states is (idle,bit_sampling,byte_processing,display_state, buffer_state);
+	signal state:  rx_states;
+	
+	type tx_states is (await, transmit);
+	signal tx_state: tx_states := await;
 	
 	
 	-------------------------- SIGNALS --------------------------
@@ -40,20 +45,30 @@ architecture UART_ARCH of UART is
 	
 	-- Status Register
 	signal UART_RX_STATUS: std_logic_vector(3 downto 0) := 	"0001";
+	
 	/*
 		0 - Wait For Startbit
 		1 - Sampling
-		2 - 
+		2 - Transmiting Data
 		3 - Read Data Ready
 	*/
+	
+	-- Tx Transmit Register
+	signal UART_TX_DATA: std_logic_vector(7 downto 0) := "00000000";
+	
+	signal UART_TX_BYTE_CNT: 	natural range 0 to 9:= 0;
+	
+	-- TX Module Counters
+	signal UART_TX_CLK_CNT:		natural := 0;
+		
 	
 	-- Clock Counter of Sampling Counter
 	signal UART_RX_SAMPLE_CNT: natural := 0;
 	
-	signal UART_RX_BYTE_CNT: 	natural range 0 to 8:= 0;
-	signal UART_RX_SMP_CNT: 	natural range 0 to 8:= 0;	
-
-	--Detect falling edge
+	signal UART_RX_BYTE_CNT: 	natural range 0 to 9:= 0;
+	signal UART_RX_SMP_CNT: 	natural range 0 to 9:= 0;	
+	
+	--Detect Falling Edge
 	signal rx_input: 	std_logic := '1';
 	signal rx_prev:	std_logic := '1';
 	
@@ -104,12 +119,16 @@ architecture UART_ARCH of UART is
 		end if;
 		
 		return output;
-		
 	end function;
 	
-
 	
 begin
+	
+	-- ###########################################################
+	-- ###																	  ###
+	-- ###							RX MODULE							  ###
+	-- ###																	  ###
+	-- ###########################################################
 	
 	-- Clock Dependent Input Detection For Clock Independent Signal
 	process(all) is
@@ -180,13 +199,13 @@ begin
 		elsif rising_edge(clk) then
 			
 			case state is				
-				--Idle State					--Reset All Registers/Values
+				-- Idle State					-- Reset All Registers/Values
 				when idle 					=> UART_RX_DATA 			<= "00000000";
 													UART_RX_SAMPLE 		<= "00000000";
 													UART_RX_BYTE_CNT 		<= 0;
 													UART_RX_SAMPLE_CNT 	<= 0;
 													
-													--BROKE ASS FALLING EDGE DETECTION
+													-- BROKE ASS FALLING EDGE DETECTION
 													if (rx_prev xor rx_input) = '1' and rx_prev = '1' then
 														UART_RX_STATUS(0) <= '0';
 														UART_RX_STATUS(1) <= '1';
@@ -195,8 +214,8 @@ begin
 													end if;
 													
 					
-				--Bit Sampling					--Loop Over Input Signal
-				when bit_sampling 		=> --If Signal Is In The Middle Of The Bit 
+				-- Bit Sampling				-- Loop Over Input Signal
+				when bit_sampling 		=> -- If Signal Is In The Middle Of The Bit 
 													if (UART_RX_SAMPLE_CNT = SAMPLE_RATE/2) and (UART_RX_SMP_CNT <= 7) then
 														
 														UART_RX_SAMPLE(UART_RX_SMP_CNT) <= rx_signal;
@@ -208,15 +227,15 @@ begin
 													end if;
 													
 													
-													--Increase/Reset Sample Counter
+													-- Increase/Reset Sample Counter
 													if UART_RX_SAMPLE_CNT = SAMPLE_RATE then
 														
-														--Reset Counter
+														-- Reset Counter
 														UART_RX_SAMPLE_CNT <= 0;
 														
-														--If Count = 7 Exit State Switch to Byte Processing
+														-- If Count = 7 Exit State Switch to Byte Processing
 														if UART_RX_SMP_CNT >= 7 then
-															--Exit Sampling State
+															-- Exit Sampling State
 															UART_RX_STATUS(1) <= '0';
 															
 															--Reset Counter
@@ -238,8 +257,8 @@ begin
 														UART_RX_STATUS(1) <= '1';
 													end if;
 				
-				--Process Byte From Bit		--Whenever Bit Recived Process Output Variable
-													--Seems To Work
+				-- Process Byte From Bit	-- Whenever Bit Recived Process Output Variable
+													-- Seems To Work
 				when byte_processing 	=>	UART_RX_DATA(UART_RX_BYTE_CNT) <= byte_value(UART_RX_SAMPLE(5 downto 2));
 													
 													UART_RX_SAMPLE <= "00000000";
@@ -257,9 +276,9 @@ begin
 														
 													end if;
 				
-				--Code For Display			--Display Hex Values
-				when display_state	 	=> display_hex1 <= display(UART_RX_DATA(3 downto 1) & UART_RX_DATA(7));
-													display_hex2 <= display(UART_RX_DATA(0) & UART_RX_DATA(6 downto 4));
+				-- Code For Display			-- Display Hex Values
+				when display_state	 	=> display_hex1 <= display(UART_RX_DATA(7 downto 4));
+													display_hex2 <= display(UART_RX_DATA(3 downto 0));
 													UART_RX_STATUS(3) <= '0';
 													
 				when others => NULL;
@@ -267,5 +286,94 @@ begin
 		end if;
 	end process;
 	
+	-- ###########################################################
+	-- ###																	  ###
+	-- ###							TX MODULE							  ###
+	-- ###																	  ###
+	-- ###########################################################
+	
+	-- Not Sure If Needed
+	tx_state_transition: process(all)
+	begin
+		if rstn = '0' then
+			
+			tx_state <= await;
+			--UART_TX_DATA <= "00000000";
+			
+			--UART_TX_BYTE_CNT <= 0;
+			
+		elsif rising_edge(clk) then
+			case tx_state is			-- If Data Ready From RX
+				when 		await 	=> if UART_RX_STATUS(3) = '1' then
+												-- Change State to Transmit
+												tx_state <= transmit;
+												
+
+											end if;
+				
+											-- If Done Transmiting Change To Await State
+				when 		transmit => if UART_RX_STATUS(2) = '0' then
+												tx_state <= await;
+											end if;
+				
+				when others 		=> NULL;
+			end case;
+		end if;
+	end process;
+	
+	-- When Data Ready Send It Out
+	tx_mod: process(all)
+	begin
+		if rstn = '0' then
+			UART_TX_DATA 	<= "00000000";
+			
+			UART_TX_BYTE_CNT <= 0;
+			
+		elsif rising_edge(clk) then
+			case tx_state is
+				
+				-- Wait For Data Ready, So Do Nothing
+				when 		await 	=> 	-- Copy Data To Send Out Over TX
+												UART_TX_DATA <= UART_RX_DATA;
+												
+												tx_signal <= '1';
+												
+												if UART_RX_STATUS(3) = '1' then
+													-- Change Status To Transmiting
+													UART_RX_STATUS(2) <= '1';
+												end if;
+				
+				-- Print Data To Tx Signal Line
+				when 		transmit => if UART_TX_CLK_CNT >= BAUD_RATE then
+												-- Reset Counter and Increase Byte Position
+												UART_TX_BYTE_CNT 	<= UART_TX_BYTE_CNT + 1;
+												UART_TX_CLK_CNT 	<= 0;
+												
+											else
+											
+												tx_signal <= UART_TX_DATA(UART_TX_BYTE_CNT);
+												
+												-- Increase Counter
+												UART_TX_CLK_CNT <= UART_TX_CLK_CNT + 1;
+												
+											end if;
+											
+											
+											
+											-- If End Reached Change Output Signal To High And Exit Transmit State
+											if UART_TX_BYTE_CNT > 7 then
+											
+												UART_RX_STATUS(2) <= '0';
+												
+												tx_signal <= '1';		
+			
+												UART_TX_CLK_CNT <= 0;
+												UART_TX_BYTE_CNT <= 0;
+											end if;
+				
+				when others 		=> NULL;
+			end case;
+		end if;
+	end process;
 
 end architecture;
